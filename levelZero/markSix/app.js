3a7abca587b7c3835d22b818008cd7249581f5f4
Ritesh Kumar <56651899+wordssaysalot@users.noreply.github.com>
2021-06-10 21:37:01 +0530

    initialize

---
+++ levelZero/markSix/app.js
@@ -0,0 +1,5 @@
+let btnTranslate = document.getElementById('btn-translate');
+let inputArea = document.getElementById('in-txt');
+let outputArea = document.getElementById('out-txt');
+
+let url = 'https://api.funtranslations.com/translate/minion.json';
